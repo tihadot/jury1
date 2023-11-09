cb00cc0ce62c15c7aea0a5046263672ddcc3195a
Timo Hardebusch <53420648+tihadot@users.noreply.github.com>
2023-11-09 18:12:12 +0100

    cleanup docker logs output, add documentation

--- src/execution/execution.controller.ts
+++ src/execution/execution.controller.ts
@@ -1,10 +1,23 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ExecutionService } from './execution.service';
 
+/**
+ * @class ExecutionController - Controller that handles the execution of code
+ */
 @Controller('execute')
 export class ExecutionController {
+
+    /**
+     * Creates an instance of ExecutionController.
+     * @param { ExecutionService } executionService - The execution service
+     */
     constructor(private readonly executionService: ExecutionService) { }
 
+    /**
+     * Runs the given python code in a docker container
+     * @param { string } code - The code to run
+     * @returns { Promise<{ output: string }> } - The output of the code
+     */
     @Post('/python')
     async executePython(@Body('code') code: string): Promise<{ output: string }> {
         const output = await this.executionService.runPythonCode(code);
--- src/execution/execution.service.ts
+++ src/execution/execution.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import * as Docker from 'dockerode';
 
+/**
+ * @class ExecutionService - Service that handles the execution of code
+ */
 @Injectable()
 export class ExecutionService {
     private docker: Docker;
 
+    /**
+     * Creates an instance of ExecutionService.
+     */
     constructor() {
         // this.docker = new Docker({ socketPath: '/var/run/docker.sock' });
         this.docker = new Docker({ host: '127.0.0.1', port: 2375 });
     }
 
+    /**
+     * Runs the given python code in a docker container
+     * @param { string } code - The code to run
+     * @returns { Promise<string> } - The output of the code
+     */
     async runPythonCode(code: string): Promise<string> {
         const container = await this.docker.createContainer({
             Image: 'python:3.12.0-alpine',
@@ -26,7 +37,7 @@ export class ExecutionService {
                 }
 
                 let data = '';
-                stream.on('data', chunk => data += chunk.toString('utf8'));
+                stream.on('data', chunk => data += this.parseOutput(chunk.toString('utf8')));
                 stream.on('end', () => resolve(data));
             });
         });
@@ -43,5 +54,25 @@ export class ExecutionService {
         await container.remove();
 
         return output;
+    }
+
+    /**
+     * Strips the first 8 characters from each line of the docker container logs (docker headers)
+     * @param { string } output - The logs of the docker container
+     * @returns { string } - The logs of the docker container without the docker headers
+     */
+    parseOutput(output: string): string {
+        const logLine = output.split('\n');
+        let result = '';
+
+        // Remove the first 8 characters from each line, add a new line (except for the last line)
+        logLine.forEach(element => {
+            element = element.slice(8);
+            if (element !== '') {
+                result += element + '\n';
+            }
+        });
+
+        return result;
     }
 }
