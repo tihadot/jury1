dd0b3e5a18ebb8b53325e325e54086297090d873
Timo Hardebusch <53420648+tihadot@users.noreply.github.com>
2023-12-28 14:29:11 +0100

    remove modules redundancy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ExecutionModule } from './execution/execution.module';
-import { PythonSanitizerService } from './python-sanitizer/python-sanitizer.service';
-import { JavaSanitizerService } from './java-sanitizer/java-sanitizer.service';
 import { JavaSanitizerModule } from './java-sanitizer/java-sanitizer.module';
 import { PythonSanitizerModule } from './python-sanitizer/python-sanitizer.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ExecutionModule, JavaSanitizerModule, PythonSanitizerModule],
   controllers: [AppController],
-  providers: [AppService, PythonSanitizerService, JavaSanitizerService],
+  providers: [AppService],
 })
 export class AppModule { }
